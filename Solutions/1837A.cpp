500ff2dcfdf2648a3e49e2ae2c5789d48c9c024b
senzawaa <olsenliang3@gmail.com>
2025-04-07 21:35:54 +0700

    main

---
+++ Solutions/1837A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, k;
+        cin >> x >> k;
+        if (x % k == 0) {
+            cout << 2 << '\n';
+            cout << (x-1) << ' ' << 1 << '\n';
+        } else cout << 1 << '\n' << x << '\n';
+    }
+}
